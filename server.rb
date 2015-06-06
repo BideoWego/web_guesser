c9ab296d08cabd5d8ec86e6adb652e4e79736463
Chris Scavello <bideowego@gmail.com>
2015-06-06 17:30:01 -0400

    I3: working guessing game

--- server.rb
+++ server.rb
@@ -1,8 +1,50 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+$secret_number
+
+def new_game
+	$secret_number = (params['guess'] == nil || $secret_number == nil) ? rand(100) : $secret_number 
+end
+
+def get_locals
+	number = $secret_number
+	response = 'Guess any number between 0 and 100'
+	correct = false
+	guess = params['guess']
+
+	if guess != nil
+		guess = guess.to_i
+
+		if guess > number
+			difference = guess - number
+			if difference > 5
+				response = "Way too high."
+			else
+				response = "Too high."
+			end
+		elsif guess < number
+			difference = number - guess
+			if difference > 5
+				response = "Way too low."
+			else
+				response = "Too low."
+			end
+		else
+			response = "Correct!"
+			correct = true
+		end
+	end
+
+	{
+		:number => number.to_s,
+		:guess => guess.to_s,
+		:response => response,
+		:correct => correct
+	}
+end
+
 get '/'  do
-	number = rand(100)
-	guess = params['guess'].to_s
-	erb :index, :locals => {:number => number.to_s, :guess => guess}
+	new_game
+	erb :index, :locals => get_locals
 end
